1798c937f78549b7ed34658987d1e2cc0ce82fc1
MarinaAnto <antonellaarmas160@gmail.com>
2022-04-11 08:08:37 +0200

    Aggiungere i file di progetto.

---
+++ CShop/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShop
+{
+    internal class Prodotto
+    {
+        //Attributi
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private double prezzo;
+        private double iva;
+
+        //Costruttore
+        public Prodotto(int codice,string nome,string descrizione,double prezzo,double iva)
+        {
+            this.codice = codice;
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+        //Metodi
+        public string GetNome()
+        {
+            return nome;
+        }
+        public string GetDescrizione()
+        {
+            return descrizione;
+        }
+        public double GetPrezzo()
+        {
+            return prezzo;
+        }
+        public double GetIVA() 
+        { 
+            return iva;
+        }
+
+        public int CodeRandom()
+        {
+            Random random = new Random();
+            int i = 0;
+            while (i < 6)
+            {
+                codice = random.Next();
+                i++;
+            }
+            Console.WriteLine(codice);
+        } 
+        public string NomeEsteso()
+        {
+            Console.WriteLine(codice + nome);
+        }
+        public double PrezzoBase()
+        {
+            Console.WriteLine(prezzo);
+        }
+        public double PrezzoIva()
+        {
+            iva = prezzo * iva;
+            Console.WriteLine(prezzo + iva);
+        }
+        
+        public void StampaTutto()
+        {
+            Console.WriteLine("Codice identificativo e nome: "+ NomeEsteso() +"\nDescrizione: "+ descrizione +"\nPrezzo Base: "+ PrezzoBase() +"\nPrezzo iva inclusa: "+ PrezzoIva());
+        }
+
+
+
+
+    }
+}
---
+++ CShop/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Nel progetto csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop.
+Un prodotto è caratterizzato da:
+codice(numero intero)
+nome
+descrizione
+prezzo
+iva
+Usate opportunamente ai modificatori di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random.
+Il codice prodotto sia accessibile solo in lettura
+Gli altri attributi siano accessibili sia in lettura che in scrittura
+Il prodotto esponga sia un metodo per avere il prezzo base
+uno per avere il prezzo comprensivo di iva
+Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+Nel vostro programma principale, testate tutte le funzionalità della classe Prodotto, creando vari prodotti e stampandone il loro contenuto.
+*/
+using CShop;
+Console.WriteLine("Inserisci il nome del prodotto");
+string nome1=Console.ReadLine();
+Console.WriteLine("Inserisci a descrizione del prodotto");
+string descrizione1 = Console.ReadLine();
+Console.WriteLine("Inserisci il prezzo del prodotto");
+double prezzo1 =int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci l'iva del prodotto");
+double iva1 = int.Parse(Console.ReadLine());
+
+
+
+Prodotto nuovoprodotto = new Prodotto();
+int codice1= nuovoprodotto.CodeRandom();
+nuovoprodotto.nome = nome1;
+nuovoprodotto.descrizione=descrizione1;
+nuovoprodotto.prezzo=prezzo1;
+nuovoprodotto. iva=iva1;
+nuovoprodotto.StampaTutto();
